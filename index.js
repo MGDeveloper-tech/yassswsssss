a203febf9af4cf6537745ba756e96840c719dae6
MGDeveloper-tech <mistery.of.mail1@gmail.com>
2025-03-08 14:57:38 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,150 @@
+// const user1 = "David";
+// const age1 = 16;
+
+// const user2 = "Mariam";
+// const age2 = 15;
+
+// const user3 = "Jane";
+// const age3 = 16;
+// const lastname3 = "White";
+
+// const Fullname = user3 + lastname3;
+
+// const str1 = "Agatha"
+// const str2 = "Harkness"
+// const str3 = str1 + str2;
+
+
+
+// const color1 = "Violet";
+// const color2 = "Purple";
+// const color3 = "Black";
+
+// const myname = "David Harkness"
+
+
+
+
+
+// const nam1 = 16;
+// const nam2 = -16;
+// const nam3 = 8 + 8
+// const nam4 = 20 - 4
+// const nam5 = 8 * 2
+// const nam6 = 32 / 2
+
+// const elem1 = document.querySelector(".hello");
+
+
+// console.log(elem1);
+
+// const elem2 = document.querySelector(".yass");
+
+// console.log(elem2)
+
+// const elem3 = document.querySelector(".Mommy");
+
+// elem2.style.color = "purple";
+// elem2.style.backgroundColor = "black";
+// elem2.style.border = "3px solid purple";
+// elem2.style.width = "300px";
+// elem2.style.height = "100px";
+
+// elem1.style.color = "Purple";
+
+// elem3.style.border = "5px solid purple";
+// elem3.style.borderradius = "40px";
+// elem3.style.width = "500px";
+
+// elem1.textContent = "A Wich";
+
+// elem2.textContent = "The Dark Witch";
+
+
+const div = document.querySelector(".mom")
+
+div.addEventListener("click", () => {
+    div.style.backgroundColor = "blue"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(nam1);
+// console.log(nam2);
+// console.log(nam3);
+// console.log(nam4);
+// console.log(nam5);
+// console.log(nam6);
+
+// console.log(color1);
+// console.log(color2);
+// console.log(color3);
+
+// console.log(user1);
+// console.log(age1);
+// console.log(user2.length);
+// console.log(age2);
+
+// console.log(str3.length);
+// console.log(myname.length);
+
+
